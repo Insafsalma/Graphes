6e2a43331a703bc7899053d557f2b5bfea5c6aaa
aminedakhli <dakhliahmedamine@gmail.com>
2020-03-30 16:51:34 +0200

    Add files via upload

--- Graphe.java
+++ Graphe.java
@@ -4,15 +4,22 @@ import java.util.Iterator;
 
 public abstract class Graphe {
 
-	private int adj [][] = new int [50][50];
-	private int fs [] = new int[50];
-	private int aps [] = new int [50];
+	protected int adj [][] = new int [50][50];
+	protected int fs [] = new int[50];
+	protected int aps [] = new int [50];
 	
 	
 	public Graphe() {
 		// TODO Auto-generated constructor stub
-		matriceAdj();
-		FsAps();
+		
+		//initialisation du Matrice d’adjacence
+		for ( int i=1; i<= Sommet.nombreSommets(); i++) {
+			for ( int j=1; j<= Sommet.nombreSommets(); j++) {
+				adj[i][j]= 0;
+			}
+		}
+		//matriceAdj();
+		//FsAps();
 	}
 	
 	public void matriceAdj(){
--- Graphe_non_oriente.java
+++ Graphe_non_oriente.java
@@ -1,20 +1,30 @@
 package graphe;
 
+import java.util.Iterator;
+
 public class Graphe_non_oriente extends Graphe {
 
 	public Graphe_non_oriente() {
 		// TODO Auto-generated constructor stub
+		super();
+		matriceAdj();
+		FsAps();
 	}
 	
 	@Override
 	public void matriceAdj(){
-        
+		Iterator iter = Arete.iterator();
+		
+        while (iter.hasNext()) {
+        	Arete a = (Arete) iter.next();
+        	super.adj[a.getOrig().getEtiquette()][a.getExtr().getEtiquette()] = a.getPoids();
+        	super.adj[a.getExtr().getEtiquette()][a.getOrig().getEtiquette()] = a.getPoids();
+        }
 	}
 	
 	@Override
 	public void FsAps() {
 		
 	}
-
 
 }
