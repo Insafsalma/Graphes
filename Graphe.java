96f9dbcaaf78b91ffb51e2b7f54fb2f5f3146392
Insafsalma <58422379+Insafsalma@users.noreply.github.com>
2020-03-31 15:32:09 +0200

    adj2fs aps

--- Graphe.java
+++ Graphe.java
@@ -12,7 +12,7 @@ public abstract class Graphe {
 	public Graphe() {
 		// TODO Auto-generated constructor stub
 		
-		//initialisation du Matrice d’adjacence
+		//initialisation du Matrice dâ€™adjacence
 		for ( int i=1; i<= Sommet.nombreSommets(); i++) {
 			for ( int j=1; j<= Sommet.nombreSommets(); j++) {
 				adj[i][j]= 0;
@@ -29,6 +29,29 @@ public abstract class Graphe {
 	public void FsAps() {
 		
 	}
-	
+	public void adj2fsaps()
+	{
+		int nbsom= adj[0][0];
+		int nbarcs=adj[0][1];
+		fs= new int[nbsom+nbarcs+1];
+		fs[0]=nbsom+nbarcs;
+		aps=new int [nbsom+1];
+		aps[0]=nbsom;
+		int k=1;
+		for(int i=1;i<=nbsom;i++)
+		{
+			aps[i]=k;
+			for(int j=1; j<=nbsom; j++)
+			{
+				if(adj[i][j]==1) 
+				{
+					fs[k]=j;
+					k++;
+				}
+			}
+			fs[k]=0;
+			k++;
+		}
+	}
 
 }
